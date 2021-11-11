56560c5658e5f7d08f52aa568c74178c73fa39ee
KonovalovIurii <konovalov_iurii@yahoo.com>
2021-11-11 21:16:40 +0300

    ДЗ№8 IO

---
+++ hw08-io/src/main/java/homework/dataprocessor/Loader.java
@@ -0,0 +1,10 @@
+package homework.dataprocessor;
+
+import homework.model.Measurement;
+
+import java.util.List;
+
+public interface Loader {
+
+    List<Measurement> load();
+}
---
+++ hw08-io/src/main/java/homework/dataprocessor/Serializer.java
@@ -0,0 +1,8 @@
+package homework.dataprocessor;
+
+import java.util.Map;
+
+public interface Serializer {
+
+    void serialize(Map<String, Double> data);
+}
